e652fc1a5ffe78c7a03d342d06bf776d8f41da29
Matt <mattelliottus@gmail.com>
2019-06-21 14:27:00 -0700

    basic scrape functionality

--- assets/js/scrape.js
+++ assets/js/scrape.js
@@ -9,41 +9,36 @@ $(document).ready(function () {
       method: "GET"
     }).then(function(data) {
       results = data.response.docs;
-      // makeDOM();
-
-      dynamicDOM.articleContainer = $(`<div>`);
-      dynamicDOM.pageTitle = $('<h1>');
-      dynamicDOM.article = $('<article>');
-      dynamicDOM.articleTitle = $('<h2>');
-      dynamicDOM.p = $(`<p>`);
-
-      
-      dynamicDOM.articleContainer.attr(`class`, `article-container`);
-      dynamicDOM.pageTitle.attr(`class`, `page-title`);
-      dynamicDOM.article.attr(`class`, `news-article`);
-      dynamicDOM.articleTitle.attr(`class`, `news-title`);
-
       placeResultsToDOM(results);
     });
   }
 
-  function makeDOM() {
-    
-  }
-
   function placeResultsToDOM(results) {
     results.forEach(function (result) {
-      console.log(result.headline.main);
-      dynamicDOM.pageTitle.text(`Search for ${query}`);
-      dynamicDOM.articleTitle.text(result.headline.main);
-      dynamicDOM.p.text(result.snippet);
-
-      dynamicDOM.articleContainer.append(dynamicDOM.pageTitle);
-      dynamicDOM.articleContainer.append(dynamicDOM.article);
-      dynamicDOM.articleContainer.append(dynamicDOM.articleTitle);
+      var articleContainer = $(`<div>`);
+      var pageTitle = $('<h1>');
+      var article = $('<article>');
+      var articleTitle = $('<h2>');
+      var p = $(`<p>`);
+  
       
+      articleContainer.attr(`class`, `article-container`);
+      pageTitle.attr(`class`, `page-title`);
+      article.attr(`class`, `news-article`);
+      articleTitle.attr(`class`, `news-title`);
 
-      $('body').append(dynamicDOM.articleContainer);
+      pageTitle.text(`Search for ${query}`);
+      articleTitle.text(result.headline.main);
+      p.text(result.snippet);
+      
+      articleContainer.append(pageTitle);
+      article.append(articleTitle);
+      article.append(p);
+      articleContainer.append(article);
+      
+      console.log(articleContainer);
+
+      $('body').append(articleContainer);
     });
   }
 
